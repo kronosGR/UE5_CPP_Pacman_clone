f8cea87eee2aef41bdc3ef3e9b3e2aadb63a788b
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-07-06 06:33:17 +0200

    added enemy class and blueprint

---
+++ Source/UE5_CPP_Pacman_clone/EnemyPawn.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "EnemyPawn.generated.h"
+
+UENUM(BlueprintType)
+enum class EEnemyState: uint8
+{
+	Default,
+	Idle
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEnemyStateChangeEvent, EEnemyState, NewState);
+
+UCLASS()
+class UE5_CPP_PACMAN_CLONE_API AEnemyPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EEnemyState State = EEnemyState::Default;
+
+	UFUNCTION(BlueprintCallable)
+	void Hunt();
+	
+	UFUNCTION(BlueprintCallable)
+	void Idle();
+
+	FEnemyStateChangeEvent& OnStateChanged(){ return StateChangedEvent;}
+
+private:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FEnemyStateChangeEvent StateChangedEvent;
+};
